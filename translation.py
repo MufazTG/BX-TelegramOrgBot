695f34ba0b2c87d976cca3b1ebdf8cd5f371b20b
MufazTG <85679056+MufazTG@users.noreply.github.com>
2021-06-11 09:56:34 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,20 +1,23 @@
 class Translation(object):
     START_TEXT = """Hi!
-🖥️How To Use This Bot🖥️ https://youtu.be/5eEsvLAKVc0 \n\nplease read the TnC before proceeding: https://t.me/Mo_Tech_YT/122
-\n👤Any Doubt @Mo_Tech_Group\n🤖Bot Update @Mo_Tech_YT\n
-Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
+Hai Iam a Simple My.telegram.org Bot.
 
-/start at any stage to re-enter your details"""
+To Get API ID & API HASH Enter your Telegram Phone Number With Country Code.
+
+👤Any Doubt @BXSupport
+🤖Bot Update @BX_Botz
+
+Click /Start To Restart The Progress"""
     AFTER_RECVD_CODE_TEXT = """I see!
-now please send the Telegram code that you received from Telegram!
+Now please send the Telegram code that you received from Telegram!
 
 this code is only used for the purpose of getting the APP ID from my.telegram.org
 if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/MRK-YT/MT-MyTelegramOrg-Bot and clicking on the Pink Button
+by opening https://github.com/MufazTG/BX-TelegramOrgBot and clicking on the Pink Button
 
-/start at any stage to re-enter your details"""
+Click /Start To Restart The Progress"""
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@Mo_Tech_Group\n\nHow Get Api Code For Website https://youtu.be/5eEsvLAKVc0"
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\nBXSupport\n\nHow Get Api Code For Website"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
     IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
     IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
